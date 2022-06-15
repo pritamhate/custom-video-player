af1bef8e4ef24d069e680c133699acaa48ff9010
pritamhate <pritam.hate@outlook.com>
2022-06-15 20:39:16 +0530

    video player functionality done

--- script.js
+++ script.js
@@ -6,34 +6,65 @@ const timestamp = document.getElementById("timestamp");
 
 // Play and Pause Video
 function toggleVideoStatus() {
-  return true;
+  //return true;
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
 // Update play/pause icon
 function updatePlayIcon() {
-  return true;
+  //return true;
+  if (video.paused) {
+    play.innerText = "Play";
+  } else {
+    play.innerText = "Pause";
+  }
 }
 
 // Update Progress & TimeStamp
 function updateProgress() {
-  return true;
+  //return true;
+  //console.log(video.currentTime);
+  //console.log(video.duration);
+
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  //get the minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  //get the seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video progress
 function setVideoProgress() {
-  return true;
+  //return true;
+  video.currentTime = (progress.value * video.duration) / 100;
 }
 
 // Stop video
 function stopVideo() {
-  return true;
+  //return true;
+  video.pause();
+  video.currentTime = 0;
 }
 
 //Event listeners
 video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
-video.addEventListener("timestamp", updateProgress);
+video.addEventListener("timeupdate", updateProgress);
 
 play.addEventListener("click", toggleVideoStatus);
 stopVid.addEventListener("click", stopVideo);
